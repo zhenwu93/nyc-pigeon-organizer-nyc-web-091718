6d9c5a6b3b31eac74975225f5a04a719ea53c37e
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 22:04:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new_pigeon_hash = {}
+
+ data.each do |property, hash|
+   hash.each do |attribute, array|
+     array.each do |name|
+       if !new_pigeon_hash.has_key?(name)
+         new_pigeon_hash[name] = {}
+       end
+
+       if !new_pigeon_hash[name].has_key?(property)
+         new_pigeon_hash[name][property] = []
+       end
+
+       if !new_pigeon_hash[name][property].include?(attribute)
+         new_pigeon_hash[name][property] << attribute.to_s
+       end
+     end
+   end
+ end
+
+ new_pigeon_hash
 end
