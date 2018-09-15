f67ac54443326bc18712760b29d8d511bee3bbc9
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 22:03:47 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,18 @@
 def nyc_pigeon_organizer(data)
-  new_hash = {}
+  new_pigeon_hash = {}
  data.each do |property, hash|
    hash.each do |attribute, array|
      array.each do |name|
-       if !new_hash.has_key?(name)
-         new_hash[name] = {}
+       if !new_pigeon_hash.has_key?(name)
+         new_pigeon_hash[name] = {}
        end
 
-       if !new_hash[name].has_key?(property)
-         new_hash[name][property] = []
+       if !new_pigeon_hash[name].has_key?(property)
+         new_pigeon_hash[name][property] = []
        end
 
-       if !new_hash[name][property].include?(attribute)
-         new_hash[name][property] << attribute.to_s
+       if !new_pigeon_hash[name][property].include?(attribute)
+         new_pigeon_hash[name][property] << attribute.to_s
        end
      end
    end
